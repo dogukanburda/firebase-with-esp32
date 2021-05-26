6d50d801a3930e0e9d050429fbb3ebd2324c8ec3
doguk <dog>
2021-05-26 22:13:03 +0300

    push adc value and continous input stream

--- firebase2.ino
+++ firebase2.ino
@@ -1,25 +1,54 @@
 #include <WiFi.h>
-#include <FirebaseESP32.h>
+#include "FirebaseESP32.h"
 
 
-#define WIFI_SSID "OURO"
-#define WIFI_PASSWORD "raspberrypi"
+#define FIREBASE_HOST "https://nosqldata-43a40-default-rtdb.firebaseio.com/" //Change to your Firebase RTDB project ID e.g. Your_Project_ID.firebaseio.com
+#define FIREBASE_AUTH "oCS30huLUeZhku6l5G2FMguGGLwrtlQswOqKRjCL" //Change to your Firebase RTDB secret password
+#define WIFI_SSID "*****"
+#define WIFI_PASSWORD "****"
 
-#define FIREBASE_HOST "nosqldata-43a40-default-rtdb.firebaseio.com/"
 
-#define FIREBASE_AUTH "oCS30huLUeZhku6l5G2FMguGGLwrtlQswOqKRjCL"
-
-//Define FirebaseESP32 data object
+//Define Firebase Data object
 FirebaseData fbdo;
+FirebaseData fbdo2;
 
-FirebaseJson json;
+const int ledPin =  19; //GPIO19 for LED
+String path = "/";
+String node = "node1";
+unsigned long eskiZaman=0;
+unsigned long yeniZaman;
 
-void printResult(FirebaseData &data);
+//Global function that handles stream data
+void streamCallback(StreamData data) // Stream's value
+{
+
+  if (data.dataType() == "int") {
+    digitalWrite(ledPin, data.intData());
+  }
 
 
+}
 
-void setup() {
-Serial.begin(115200);
+
+void streamTimeoutCallback(bool timeout)
+{
+  if (timeout)
+  {
+    Serial.println();
+    Serial.println("Stream timeout, resume streaming...");
+    Serial.println();
+  }
+}
+
+
+void setup()
+{
+
+  Serial.begin(115200);
+
+  pinMode(ledPin, OUTPUT);
+  Serial.println();
+  Serial.println();
 
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   Serial.print("Connecting to Wi-Fi");
@@ -35,165 +64,34 @@ Serial.begin(115200);
 
   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
   Firebase.reconnectWiFi(true);
-  //Set database read timeout to 1 minute (max 15 minutes)
-  Firebase.setReadTimeout(fbdo, 1000 * 60);
-  //tiny, small, medium, large and unlimited.
-  //Size and its write timeout e.g. tiny (1s), small (10s), medium (30s) and large (60s).
-  Firebase.setwriteSizeLimit(fbdo, "tiny");
 
-  
-  String path = "/Dogukan";
 
-  
-  Serial.println("------------------------------------");
-  Serial.println("Writing Double");
-  Firebase.setDouble(fbdo, path + "/analog/Data" , 3.1415926);
-  //Serial.println("PASSED");
-  Serial.println("PATH: " + fbdo.dataPath());
-  Serial.println("TYPE: " + fbdo.dataType());
-  Serial.println("ETag: " + fbdo.ETag());
-  Serial.print("VALUE: ");
-  printResult(fbdo);
-  Firebase.get(fbdo, path + "/analog/Data");
-  Serial.println("PASSED");
-  Serial.println("PATH: " + fbdo.dataPath());
-  Serial.println("TYPE: " + fbdo.dataType());
-  Serial.println("ETag: " + fbdo.ETag());
-  Serial.print("VALUE: ");
-  printResult(fbdo);
-  
+
+  if (!Firebase.beginStream(fbdo, path + "HV" +"/"+ node)) // to subscribe to the stream changes at a defined database path
+  {
+    Serial.println("Could not begin stream");
+    Serial.println("REASON: " + fbdo.errorReason());
+    Serial.println();
+  }
+
+  Firebase.setStreamCallback(fbdo, streamCallback, streamTimeoutCallback); //streamCallback is doer
 
 }
-void printResult(FirebaseData &data)
+
+void loop()
 {
-
-  if (data.dataType() == "int")
-    Serial.println(data.intData());
-  else if (data.dataType() == "float")
-    Serial.println(data.floatData(), 5);
-  else if (data.dataType() == "double")
-    printf("%.9lf\n", data.doubleData());
-  else if (data.dataType() == "boolean")
-    Serial.println(data.boolData() == 1 ? "true" : "false");
-  else if (data.dataType() == "string")
-    Serial.println(data.stringData());
-  else if (data.dataType() == "json")
-  {
-    Serial.println();
-    FirebaseJson &json = data.jsonObject();
-    //Print all object data
-    Serial.println("Pretty printed JSON data:");
-    String jsonStr;
-    json.toString(jsonStr, true);
-    Serial.println(jsonStr);
-    Serial.println();
-    Serial.println("Iterate JSON data:");
-    Serial.println();
-    size_t len = json.iteratorBegin();
-    String key, value = "";
-    int type = 0;
-    for (size_t i = 0; i < len; i++)
-    {
-      json.iteratorGet(i, type, key, value);
-      Serial.print(i);
-      Serial.print(", ");
-      Serial.print("Type: ");
-      Serial.print(type == FirebaseJson::JSON_OBJECT ? "object" : "array");
-      if (type == FirebaseJson::JSON_OBJECT)
-      {
-        Serial.print(", Key: ");
-        Serial.print(key);
-      }
-      Serial.print(", Value: ");
-      Serial.println(value);
+  yeniZaman = millis();
+  
+  if(yeniZaman-eskiZaman > 1000){
+    
+    int adc = analogRead(36);  //Reads pin 36
+    if (Firebase.pushInt(fbdo2, path +"ADC/"+node, adc)) {
+      Serial.println("pushed ADC value is "+adc);
+    } else {
+      Serial.println("Failed because; REASON: " + fbdo2.errorReason());
     }
-    json.iteratorEnd();
-  }
-  else if (data.dataType() == "array")
-  {
-    Serial.println();
-    //get array data from FirebaseData using FirebaseJsonArray object
-    FirebaseJsonArray &arr = data.jsonArray();
-    //Print all array values
-    Serial.println("Pretty printed Array:");
-    String arrStr;
-    arr.toString(arrStr, true);
-    Serial.println(arrStr);
-    Serial.println();
-    Serial.println("Iterate array values:");
-    Serial.println();
-    for (size_t i = 0; i < arr.size(); i++)
-    {
-      Serial.print(i);
-      Serial.print(", Value: ");
+    
+    eskiZaman = yeniZaman;
 
-      FirebaseJsonData &jsonData = data.jsonData();
-      //Get the result data from FirebaseJsonArray object
-      arr.get(jsonData, i);
-      if (jsonData.typeNum == FirebaseJson::JSON_BOOL)
-        Serial.println(jsonData.boolValue ? "true" : "false");
-      else if (jsonData.typeNum == FirebaseJson::JSON_INT)
-        Serial.println(jsonData.intValue);
-      else if (jsonData.typeNum == FirebaseJson::JSON_FLOAT)
-        Serial.println(jsonData.floatValue);
-      else if (jsonData.typeNum == FirebaseJson::JSON_DOUBLE)
-        printf("%.9lf\n", jsonData.doubleValue);
-      else if (jsonData.typeNum == FirebaseJson::JSON_STRING ||
-               jsonData.typeNum == FirebaseJson::JSON_NULL ||
-               jsonData.typeNum == FirebaseJson::JSON_OBJECT ||
-               jsonData.typeNum == FirebaseJson::JSON_ARRAY)
-        Serial.println(jsonData.stringValue);
-    }
-  }
-  else if (data.dataType() == "blob")
-  {
-
-    Serial.println();
-
-    for (size_t i = 0; i < data.blobData().size(); i++)
-    {
-      if (i > 0 && i % 16 == 0)
-        Serial.println();
-
-      if (i < 16)
-        Serial.print("0");
-
-      Serial.print(data.blobData()[i], HEX);
-      Serial.print(" ");
-    }
-    Serial.println();
-  }
-  else if (data.dataType() == "file")
-  {
-
-    Serial.println();
-
-    File file = data.fileStream();
-    int i = 0;
-
-    while (file.available())
-    {
-      if (i > 0 && i % 16 == 0)
-        Serial.println();
-
-      int v = file.read();
-
-      if (v < 16)
-        Serial.print("0");
-
-      Serial.print(v, HEX);
-      Serial.print(" ");
-      i++;
-    }
-    Serial.println();
-    file.close();
-  }
-  else
-  {
-    Serial.println(data.payload());
-  }
 }
-void loop() {
-  // put your main code here, to run repeatedly:
-
 }
